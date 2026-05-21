d30d1a76ff0cfe17fbd1ac90624149ce362d8689
Carlos <carlos.morisson@sesims.com.br>
2026-05-21 00:30:13 -0400

    efeito de audio quando pega algum poder

--- VR-TEMPLATE-2025/Assets/Scripts/Effects/FreezeEfffect.cs
+++ VR-TEMPLATE-2025/Assets/Scripts/Effects/FreezeEfffect.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
+using DG.Tweening;
+using NUnit.Framework.Constraints; // Importante para o DOTween funcionar
 
 public class FreezeEffect : MonoBehaviour
 {
@@ -9,19 +12,35 @@ public class FreezeEffect : MonoBehaviour
     [SerializeField] private float freezeTimeScale = 0.05f;
 
     [Header("Timing")]
-    [SerializeField] private float slowDownDuration = 1f;   
-    [SerializeField] private float freezeDuration = 7f;     
-    [SerializeField] private float restoreDuration = 0.5f;   
+    [SerializeField] private float slowDownDuration = 1f;
+    [SerializeField] private float freezeDuration = 7f;
+    [SerializeField] private float restoreDuration = 0.5f;
 
+    [Header("UI")]
+    [SerializeField] private GameObject FreezeTimeObject;
+    [SerializeField] private Image FreezeClookImage; 
 
     public UnityEvent OnStartFreezeTime;
     public UnityEvent OnFinishFreezeTime;
 
     private Coroutine freezeRoutine;
+    private Tween objectScaleTween;
+    private Tween clockFillTween;
+
+    private const string TRANSITION_AUDIO = "TransitionEffect";
 
     private void Start()
     {
         Time.timeScale = defaultTimeScale;
+
+        if (FreezeTimeObject != null)
+        {
+            FreezeTimeObject.transform.localScale = Vector3.zero;
+        }
+        if (FreezeClookImage != null)
+        {
+            FreezeClookImage.fillAmount = 0f;
+        }
     }
 
     [ContextMenu("teste")]
@@ -30,21 +49,58 @@ public class FreezeEffect : MonoBehaviour
         if (freezeRoutine != null)
             StopCoroutine(freezeRoutine);
 
+        objectScaleTween?.Kill();
+        clockFillTween?.Kill();
+
         freezeRoutine = StartCoroutine(FreezeRoutine());
     }
 
     private IEnumerator FreezeRoutine()
     {
         yield return LerpTimeScale(defaultTimeScale, freezeTimeScale, slowDownDuration);
+        AudioController.Instance.Play(TRANSITION_AUDIO);
+        if (AudioEffect.Instance != null)
+        {
+            AudioEffect.Instance.ChangeAudioPreset(AudioEffect.EffectType.DistortedSlow);
+        }
+
+        if (FreezeTimeObject != null)
+        {
+            FreezeTimeObject.SetActive(true);
+            objectScaleTween = FreezeTimeObject.transform.DOScale(Vector3.one, 0.3f)
+                .SetUpdate(true)
+                .SetEase(Ease.OutBack);
+        }
+
+        if (FreezeClookImage != null)
+        {
+            FreezeClookImage.fillAmount = 0f;
+            clockFillTween = FreezeClookImage.DOFillAmount(1f, freezeDuration)
+                .SetUpdate(true) 
+                .SetEase(Ease.Linear); 
+        }
 
         OnStartFreezeTime?.Invoke();
 
         yield return new WaitForSecondsRealtime(freezeDuration);
 
+        if (FreezeTimeObject != null)
+        {
+            objectScaleTween = FreezeTimeObject.transform.DOScale(Vector3.zero, restoreDuration)
+                .SetUpdate(true)
+                .SetEase(Ease.InBack)
+                .OnComplete(() => FreezeTimeObject.SetActive(false));
+        }
+
+        if (AudioEffect.Instance != null)
+        {
+            AudioEffect.Instance.ChangeAudioPreset(AudioEffect.EffectType.Normal);
+        }
+
         yield return LerpTimeScale(freezeTimeScale, defaultTimeScale, restoreDuration);
 
         OnFinishFreezeTime?.Invoke();
-
+        AudioController.Instance.Play(TRANSITION_AUDIO);
         freezeRoutine = null;
     }
 
@@ -57,8 +113,7 @@ public class FreezeEffect : MonoBehaviour
             time += Time.unscaledDeltaTime;
 
             float t = time / duration;
-
-            t = t * t * (3f - 2f * t); 
+            t = t * t * (3f - 2f * t);
 
             Time.timeScale = Mathf.Lerp(from, to, t);
 
@@ -66,5 +121,11 @@ public class FreezeEffect : MonoBehaviour
         }
 
         Time.timeScale = to;
+    }
+
+    private void OnDestroy()
+    {
+        objectScaleTween?.Kill();
+        clockFillTween?.Kill();
     }
 }
--- VR-TEMPLATE-2025/Assets/Scripts/Effects/PowerEffect.cs
+++ VR-TEMPLATE-2025/Assets/Scripts/Effects/PowerEffect.cs
@@ -1,43 +1,130 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class PowerEffect : MonoBehaviour
 {
     public static PowerEffect Instance { get; private set; }
-    public float PowerDuration;
 
-    public bool isPowered=false;
+    [Header("Timing")]
+    public float PowerDuration = 7f;
+    [SerializeField] private float restoreDuration = 0.5f;
+
+    [Header("UI")]
+    [SerializeField] private GameObject FreezeTimeObject;
+    [SerializeField] private Image FreezeClookImage;
+
+    [HideInInspector] public bool isPowered = false;
+
+    [Header("Events")]
     public UnityEvent OnStartPowerTime;
     public UnityEvent OnFinishPowerTime;
 
     private Coroutine powerRoutine;
+    private Tween objectScaleTween;
+    private Tween clockFillTween;
+
+    private const string TRANSITION_AUDIO = "TransitionEffect";
+
     private void Awake()
     {
-        Instance = this;
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
+
+    private void Start()
+    {
+        if (FreezeTimeObject != null)
+        {
+            FreezeTimeObject.transform.localScale = Vector3.zero;
+        }
+        if (FreezeClookImage != null)
+        {
+            FreezeClookImage.fillAmount = 0f;
+        }
+    }
+
     [ContextMenu("teste")]
     public void TriggerPower()
     {
         if (powerRoutine != null)
             StopCoroutine(powerRoutine);
 
+        objectScaleTween?.Kill();
+        clockFillTween?.Kill();
+
         powerRoutine = StartCoroutine(FreezeRoutine());
     }
 
     private IEnumerator FreezeRoutine()
     {
+        isPowered = true;
+
+        if (AudioController.Instance != null)
+        {
+            AudioController.Instance.Play(TRANSITION_AUDIO);
+        }
+
+        if (AudioEffect.Instance != null)
+        {
+            AudioEffect.Instance.ChangeAudioPreset(AudioEffect.EffectType.DarkSpace);
+        }
+
+        if (FreezeTimeObject != null)
+        {
+            FreezeTimeObject.SetActive(true);
+            objectScaleTween = FreezeTimeObject.transform.DOScale(Vector3.one, 0.3f)
+                .SetUpdate(true)
+                .SetEase(Ease.OutBack);
+        }
+
+        if (FreezeClookImage != null)
+        {
+            FreezeClookImage.fillAmount = 0f;
+            clockFillTween = FreezeClookImage.DOFillAmount(1f, PowerDuration)
+                .SetUpdate(true)
+                .SetEase(Ease.Linear);
+        }
 
         OnStartPowerTime?.Invoke();
-        isPowered = true;
 
         yield return new WaitForSecondsRealtime(PowerDuration);
 
+        if (FreezeTimeObject != null)
+        {
+            objectScaleTween = FreezeTimeObject.transform.DOScale(Vector3.zero, restoreDuration)
+                .SetUpdate(true)
+                .SetEase(Ease.InBack)
+                .OnComplete(() => FreezeTimeObject.SetActive(false));
+        }
+
+        if (AudioEffect.Instance != null)
+        {
+            AudioEffect.Instance.ChangeAudioPreset(AudioEffect.EffectType.Normal);
+        }
 
         OnFinishPowerTime?.Invoke();
+
+        if (AudioController.Instance != null)
+        {
+            AudioController.Instance.Play(TRANSITION_AUDIO);
+        }
 
         powerRoutine = null;
         isPowered = false;
     }
 
+    private void OnDestroy()
+    {
+        objectScaleTween?.Kill();
+        clockFillTween?.Kill();
+    }
 }
